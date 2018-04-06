98959b842f3614de6432f82339650d9db826e16b
altarit <mail@altarit.com>
2018-04-07 00:54:52 +0300

    Customized InputScreen editable area

--- src/components/screenComponents/InputScreen/component/InputScreen.js
+++ src/components/screenComponents/InputScreen/component/InputScreen.js
@@ -6,7 +6,15 @@ class InputScreen extends Component {
   render() {
     return (
       <div className='InputScreen Screen'>
-        InputScreen
+        <div className='InputScreen_header'>
+          <h2>InputScreen</h2>
+        </div>
+        <div className='InputScreen_form'>
+          <div contentEditable='true' className='InputScreen_textarea'/>
+        </div>
+        <div className='InputScreen_footer'>
+          Place link above
+        </div>
       </div>
     )
   }
