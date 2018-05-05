e4efce686c7d88244cbc70c00744bfa7914a3728
altarit <mail@altarit.com>
2018-05-05 15:02:15 +0300

    WIP: playlists support.

--- src/components/PreviewEntry/component/PreviewEntry.js
+++ src/components/PreviewEntry/component/PreviewEntry.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 
-import OnOffSwitch from '../../ui/OnOffSwitch'
 import './PreviewEntry.css'
 
 class PreviewEntry extends Component {
@@ -24,30 +23,38 @@ class PreviewEntry extends Component {
     let entry = this.props.entry
     return (
       <div className={'PreviewEntry' + (entry.enabled ? '' : ' PreviewEntry__disabled')}>
-        <div className='PreviewEntry_thumbnail'>
-          <a href={entry.url} target='_blank'>
-            {entry.thumbnail ? (
-              <img src={entry.thumbnail} width='100%'/>
-            ) : <p>'None'</p>}
-          </a>
-        </div>
-        <div className='PreviewEntry_info'>
-          {!entry.title ? (
-            <div className='PreviewEntry_link'><a href={entry.url} target='_blank'>{entry.url}</a></div>
-          ) : (
-            <div className='PreviewEntry_title'>{entry.title}</div>
-          )}
-          <div className='PreviewEntry_progressbar'>{entry.statusText || entry.status.name}</div>
-          <div className='PreviewEntry_control'>
-            {entry.status.id === 3 ? (
-              <button onClick={this.startProcessing}>S</button>
-            ) : null}
-            {entry.href ? (
-              <a className='btn btn-small' href={entry.href} download>Download</a>
-            ) : null}
-            <button className='btn btn-small' onClick={this.remove}>Remove</button>
+        <div className='PreviewEntry_current'>
+          <div className='PreviewEntry_thumbnail'>
+            <a href={entry.url} target='_blank'>
+              {entry.thumbnail ? (
+                <img src={entry.thumbnail} className='PreviewEntry_thumbnail-image'/>
+              ) : <p>'None'</p>}
+            </a>
+          </div>
+          <div className='PreviewEntry_info'>
+            {!entry.title ? (
+              <div className='PreviewEntry_link'><a href={entry.url} target='_blank'>{entry.url}</a></div>
+            ) : (
+              <div className='PreviewEntry_title'>{entry.title}</div>
+            )}
+            <div className='PreviewEntry_progressbar'>{entry.statusText || entry.status.name}</div>
+            <div className='PreviewEntry_control'>
+              {entry.status.id === 3 ? (
+                <button onClick={this.startProcessing}>S</button>
+              ) : null}
+              {entry.href ? (
+                <a className='btn btn-small' href={'http://localhost:80/' + entry.href} target='_blank'
+                   download>Download</a>
+              ) : null}
+              <button className='btn btn-small' onClick={this.remove}>Remove</button>
+            </div>
           </div>
         </div>
+        {entry.children ? entry.children.map(el =>
+          <PreviewEntry key={el.id} entry={el}
+                        togglePreview={()=>{}}
+                        startProcessing={()=>{}}/>
+        ) : null}
       </div>
     )
   }
